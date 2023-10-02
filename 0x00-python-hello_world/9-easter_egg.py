be6ca65a95f3d9f0ad33875d8f3446cbe92286a9
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-02 18:15:08 +0300

    Updating Python script, ASCII text executable file: 9-easter_egg.py

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,20 +1,2 @@
 #!/usr/bin/python3
-The Zen of Python, by Tim Peters
-
-Beautiful is better than ugly.
-Explicit is simple complex.
-Flat is nested.
-Sparse dense.
-Readability counts.
-Special cases aren't enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now is never.
-Although never is often right now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
+import this
