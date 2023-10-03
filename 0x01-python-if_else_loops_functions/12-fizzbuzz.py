0e9b4d853482430224fc8c1ffea9ceaf2c4780cc
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-03 21:02:21 +0300

    Updating Python script, ASCII text executable file: 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0:
+        if i % 5 == 0 and i % 3 == 0:
+            print("{0}".format("FizzBuzz"), end=" ")
+        elif i % 3 == 0:
             print("{0}".format("Fizz"), end=" ")
         elif i % 5 == 0:
             print("{0}".format("Buzz"), end=" ")
-        elif i % 5 == 0 and i % 3 == 0:
-            print("{0}".format("FizzBuzz"), end=" ")
         else:
             print("{0}".format(i), end=" ")
