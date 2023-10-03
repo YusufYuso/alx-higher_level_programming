21faa7bdd068b391d2e61011d40d19355ed1ed4e
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-03 23:07:38 +0300

    Adding Python script, ASCII text executable file: 101-remove_char_at.py

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    if n > len(str) or n < 0:
+        return str
+    else:
+        for i in str:
+            if i == str[n]:
+                return str[:n] + str[n + 1 :]
