cb894860ccb9ca5e3a56d83d5a0b1cc999643e45
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-03 19:22:39 +0300

    Updating Python script, ASCII text executable file: 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase(str):
+    string = ""
+    for char in str:
+        if ord(char) in range(97, 123):
+            check = ord(char) - 32
+            string = string + chr(check)
+        elif ord(char) == 32:
+            string = string + " "
+        else:
+            check = ord(char)
+            string = string + chr(check)
+    print(string)
