567fb460d9723be1960cfe9c23fd045d9515cd0c
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-05 14:19:07 +0300

    Updating Python script, ASCII text executable file: 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         print(".\n")
     elif len(args) == 1:
         print("{} argument:".format(len(args)))
-        print("{}: {}\n".format(len(args), args[0]))
+        print("{}: {}".format(len(args), args[0]))
     else:
         print("{} arguments:".format(len(args)))
         for i, value in enumerate(args):
