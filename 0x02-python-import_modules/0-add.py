990a53b0d8482613079a8da550ee7f6dc33b4919
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-05 13:25:28 +0300

    Updating Python script, ASCII text executable file: 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    import add_0 as a
 
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    print("{} + {} = {}".format(a, b, a.add(a, b)))
