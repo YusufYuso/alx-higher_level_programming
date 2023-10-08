3c8e0c84747ec0aca4ce4973904ca565e805419b
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-08 10:07:15 +0300

    Updating Python script, ASCII text executable file: 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     """Print all integers of a list."""
     for idx in range(len(my_list)):
-        print("{0:d}".format(my_list[idx]))
+        print("{:d}".format(my_list[idx]))
