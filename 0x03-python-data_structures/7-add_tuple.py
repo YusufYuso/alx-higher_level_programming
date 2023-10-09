d0eb17f63c7ea2736eb0b574bcf3a626003d6500
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-09 12:09:04 +0300

    Updating Python script, ASCII text executable file: 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) > 2 or len(tuple_b) > 2:
+        tuple_a = tuple_a[:2]
+        tuple_b = tuple_b[:2]
+    if len(tuple_a) < 2:
+        missing_elements_a = 2 - len(tuple_a)
+        zeros = (0,) * missing_elements_a
+        tuple_a = tuple_a + zeros
+    if len(tuple_b) < 2:
+        missing_elements_b = 2 - len(tuple_b)
+        zeros = (0,) * missing_elements_b
+        tuple_b = tuple_b + zeros
+    result = tuple([x + y for x, y in zip(tuple_a, tuple_b)])
+    return result
