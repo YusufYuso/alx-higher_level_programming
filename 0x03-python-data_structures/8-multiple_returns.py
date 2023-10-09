18b327c10125bc9070ec3d01e0e66abbd0e3f6c7
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-09 12:16:46 +0300

    Updating Python script, ASCII text executable file: 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 def multiple_returns(sentence):
     first_char = sentence[0]
     if len(sentence) == 0:
-        first_char = None
+        first_char = "None"
     return (len(sentence), first_char)
