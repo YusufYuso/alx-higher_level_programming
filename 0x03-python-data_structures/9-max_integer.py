db6f377f78db4d8b5bcad84fad5fffbcc0470156
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-09 14:51:45 +0300

    Updating Python script, ASCII text executable file: 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = 0
+    max_item = 0
     if not my_list:
         return None
     for i in my_list:
-        if i > max:
-            max = i
-    return max
+        if i > max_item:
+            max_item = i
+    return max_item
