b412efd3fd000e0b03c82ac86356fec9dca186ac
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-12 00:02:35 +0300

    Updating Python script, ASCII text executable file: 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,3 +2,17 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or None:
         return 0
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+    result = 0
+    prev = 0
+
+    for key in roman_string[::-1]:
+        current = roman[key]
+        if current < prev:
+            result -= current
+        else:
+            result += current
+        prev = current
+
+    return result
