9030922ec571383ac382eff715108fcc882ed37e
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-12 01:04:34 +0300

    Updating Python script, ASCII text executable file: 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,3 +2,8 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
+    integers , weights = zip(*my_list)
+    weighted_sum = sum(x * w for x, w in zip(integers, weights))
+    total = sum(weights)
+    average = weighted_sum / total
+    return average
