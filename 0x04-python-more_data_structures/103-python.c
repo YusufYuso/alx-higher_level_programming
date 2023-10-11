ce405d3b3a9a7cbdc8fec3df52157b995a4a9f1b
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-12 01:57:25 +0300

    Updating C source, ASCII text file: 103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,4 +1,28 @@
 #include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list - Print information about a Python list object.
+ * @p: A pointer to the Python list object to be printed.
+ */
+void print_python_list(PyObject *p) {
+    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t i;
+
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++) {
+        PyObject *item = PyList_GetItem(p, i);
+        const char *typeName = Py_TYPE(item)->tp_name;
+
+        printf("Element %ld: %s\n", i, typeName);
+        if (PyBytes_Check(item)) {
+            print_python_bytes(item);
+        }
+    }
+}
 
 /**
  * print_python_bytes - Print information about a Python bytes object.
