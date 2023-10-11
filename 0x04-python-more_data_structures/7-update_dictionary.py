5037f4c3a3331be6d8b597e04bc004f2db83d6cf
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-11 16:37:59 +0300

    Updating Python script, ASCII text executable file: 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if not key in a_dictionary:
+    if key not in a_dictionary:
         a_dictionary.update({key: value})
     else:
         a_dictionary[key] = value
