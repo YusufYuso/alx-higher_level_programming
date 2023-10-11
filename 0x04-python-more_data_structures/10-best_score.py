6dc91e351b798a950cd7e71240dace70c7b8ed21
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-11 20:17:23 +0300

    Updating Python script, ASCII text executable file: 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or a_dictionary is None:
+    if a_dictionary is None:
         return None
-    return max(a_dictionary)
+    return max(a_dictionary.keys())
