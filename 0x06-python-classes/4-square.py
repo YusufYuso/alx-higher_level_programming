75bdc11039f3a1ccf83a7a63c10c2798686e32cc
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-24 16:39:20 +0300

    Updating Python script, ASCII text executable file: 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,6 +9,16 @@ as well as calculating its area.
 
 
 class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        area(): Calculate the area of the square.
+    """
+
     def __init__(self, size=0):
         """
         Initializes a Square instance with a specified size.
@@ -34,6 +44,16 @@ class Square:
 
     @size.setter
     def size(self, value):
+        """
+        Set the size of the square.
+
+        Args:
+            value (int): The size to set.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
