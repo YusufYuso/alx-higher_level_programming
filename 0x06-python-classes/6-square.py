c377cfa555d5782c8da09ab7eecd2381ac68a157
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-24 21:20:09 +0300

    Updating Python script, ASCII text executable file: 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,7 +51,8 @@ class Square:
         """Print the square with the # character."""
         if self.__size == 0:
             print()
-        for i in range(self.__position[1]):
-            print()
-        for j in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for j in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
