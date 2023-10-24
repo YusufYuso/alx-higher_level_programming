94cfa31e2867d23436dd4594377d209614a88282
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-24 21:18:26 +0300

    Updating Python script, ASCII text executable file: 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,12 +3,27 @@
 
 
 class Square:
-    def __init__(self, size=0):
+    """Represent a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
         self.__size = size
+        self.__position = position
 
     @property
     def size(self):
+        """Get/set the current size of the square."""
         return self.__size
+
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return self.__position
 
     @size.setter
     def size(self, value):
@@ -18,13 +33,25 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @position.setter
+    def position(self, value):
+        if (
+            not isinstance(value, tuple)
+            or len(value) != 2
+            or not all(isinstance(x, int) and x >= 0 for x in value)
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
+        """Return the current area of the square."""
         return self.__size**2
 
     def my_print(self):
+        """Print the square with the # character."""
         if self.__size == 0:
             print()
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
+        for i in range(self.__position[1]):
             print()
+        for j in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
