89c5b26a11104646d21ce776c474edeccc358114
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-24 14:28:59 +0300

    Adding Python script, ASCII text executable file: 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(size): Initializes a Square instance with a specified size.
+
+    Args:
+        size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with a specified size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
