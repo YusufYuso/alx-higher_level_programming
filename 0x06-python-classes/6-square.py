0dd02bfc2f1006a77520e95eff8104d5615f0010
YusufYuso <yusuf.elsaed@gmail.com>
2023-10-24 21:21:47 +0300

    Updating Python script, ASCII text executable file: 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -20,11 +20,6 @@ class Square:
         """Get/set the current size of the square."""
         return self.__size
 
-    @property
-    def position(self):
-        """Get/set the current position of the square."""
-        return self.__position
-
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -32,6 +27,11 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return self.__position
 
     @position.setter
     def position(self, value):
