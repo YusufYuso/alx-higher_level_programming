e2f94bed7815e87e9baff5dd12a07a0ec8e1f5fa
YusufYuso <yusuf.elsaed@gmail.com>
2024-06-18 19:14:32 +0300

    Updating a /usr/bin/node script, ASCII text executable file: 3-value_argument.js

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+//a script that prints the first argument passed to it
+const args = process.argv.slice(2);
+console.log(typeof args[0] === 'undefined' ? 'No argument' : args[0]);
