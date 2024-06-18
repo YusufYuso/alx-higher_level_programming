7e0d9162244fb8f62be1c87aa4172878b81b2852
YusufYuso <yusuf.elsaed@gmail.com>
2024-06-18 19:33:53 +0300

    Updating a /usr/bin/node script, ASCII text executable file: 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,9 +5,9 @@
 const args = process.argv.slice(2);
 
 if (args.length < 2) {
-    console.log(0);
-  } else {
-    const numbers = args.map(Number)
-      .sort((a, b) => b - a);
-    console.log(numbers[1]);
-  }
+  console.log(0);
+} else {
+  const numbers = args.map(Number)
+    .sort((a, b) => b - a);
+  console.log(numbers[1]);
+}
