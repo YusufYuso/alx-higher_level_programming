e6c73d8a13013d4a74c46d084a1af60c900cd58f
YusufYuso <yusuf.elsaed@gmail.com>
2024-06-18 19:14:36 +0300

    Updating a /usr/bin/node script, ASCII text executable file: 5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+//a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer
+const args = process.argv.slice(2);
+
+const num = parseInt(args[0], 10);
+// Convert the first argument to an integer using base 10
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
