0f24584b4eaad8906408e68baac51e5867d5ecb1
YusufYuso <yusuf.elsaed@gmail.com>
2024-06-28 17:15:37 +0300

    Updating Python script, ASCII text executable file: 0x0F-python-object_relational_mapping/0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,12 +12,17 @@ if __name__ == '__main__':
     Access to the database and get the states
     from the database.
     """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+    connection = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states")
+    states = cursor.fetchall()
 
-    for row in rows:
-        print(row)
+    for state in states:
+        print(state)
+        
+    if cursor:
+        cursor.close()
+    if connection:
+        connection.close()
