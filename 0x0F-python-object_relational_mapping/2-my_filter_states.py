fd95b35d5f8af8a5cf297ea8a3cca04064b25ef0
YusufYuso <yusuf.elsaed@gmail.com>
2024-06-28 17:15:40 +0300

    Updating Python script, ASCII text executable file: 0x0F-python-object_relational_mapping/2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,14 +15,19 @@ if __name__ == '__main__':
     from the database.
     """
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+    connection = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states \
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states \
                  WHERE name LIKE BINARY '{}' \
                  ORDER BY states.id ASC".format(argv[4]))
-    rows = cur.fetchall()
+    states = cursor.fetchall()
 
-    for row in rows:
-        print(row)
+    for state in states:
+        print(state)
+        
+    if cursor:
+        cursor.close()
+    if connection:
+        connection.close()
